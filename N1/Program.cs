f699a275a643597398fd745b7b5805245d701e12
Алексей Викулин <vaa@megamade.ru>
2022-07-26 13:35:20 +0300

    Add project files.

---
+++ N1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N1 //Два треугольника заданы длинами своих сторон. Определить, площадь какого из них больше (создать метод для вычисления  площади  треугольника по длинам его сторон).  Для решения задачи можно использовать формулу Герона (формула) , где x, y, z – стороны треугольника, p – полупериметр.
+
+
+{
+    internal class Program
+    {
+        static double GetS(double a, double b, double c)
+        {
+            double p = (a + b + c)/2;
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите стороны первого треугольника");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            double y1 = Convert.ToDouble(Console.ReadLine());
+            double z1 = Convert.ToDouble(Console.ReadLine());
+            double s1 = GetS(x1, y1, z1);
+            Console.WriteLine("Введите стороны первого треугольника");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            double y2 = Convert.ToDouble(Console.ReadLine());
+            double z2 = Convert.ToDouble(Console.ReadLine());
+            double s2 = GetS(x2, y2, z2);
+
+            if (s1 > s2)
+                Console.WriteLine("Площадь первого треугольника больше");
+            else
+                if (s2 > s1)
+                Console.WriteLine("Площадь второго треугольника больше");
+            else
+                Console.WriteLine("Площади треугольников равны");
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ N2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N2
+{
+    internal class Program
+    {
+        static void GetVSCube (double a, out double s, out double v)
+        {
+            v = a * a * a;
+            s = a * a * 6;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину ребра куба:");
+            double A = Convert.ToDouble(Console.ReadLine());
+            double V;
+            double S;
+            GetVSCube (A, out V, out S);
+            Console.WriteLine("{0:0.00} {1:0.00}", S, V);
+            Console.ReadLine();
+        }
+    }
+}
